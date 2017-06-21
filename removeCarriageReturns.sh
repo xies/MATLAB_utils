00e3c3e2fcbaeba2b0f79f962a747be789694db3
xies <xies@mit.edu>
2017-06-21 16:16:51 -0700

    moved stuff out of MATLAB

--- removeCarriageReturns.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# script to remove carriage returns from files (useful for processing
-# excel tab-delimited files). use:
-# removeCarriageReturn.sh FILENAME
-
-tr "\r" "\n" < $1 > $1.ncr
